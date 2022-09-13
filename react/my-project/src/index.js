95657203ecf12bf90cf1aab2fd65c5d0c8c16c72
Sraban Pahadasingh <sraban.pahadasingh@wisseninfotech.com>
2022-09-13 18:04:33 +0530

    test cases

--- react/my-project/src/index.js
+++ react/my-project/src/index.js
@@ -11,6 +11,7 @@ import store from './redux/store';
 import { createBrowserHistory } from 'history';
 export const history = createBrowserHistory();
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store} history={history}>
