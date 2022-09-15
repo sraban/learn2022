57dee1b7226a757d5cda00a85de59cd96435a754
sraban <sraban307@gmail.com>
2022-09-15 09:24:22 +0530

    done

--- react/my-project/src/pages/report/report.js
+++ react/my-project/src/pages/report/report.js
@@ -1,25 +1,111 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './report.scss';
+import { useState, useEffect, useRef } from 'react';
 
 import Grandparent from './../../components/com/grandparent/grandparent.js';
 
 import UserContextProvider from './../../context/index1.js';
 
-const Report = () => (
-  <div className="report">
-    Report Component
+
+import * as APIService from './api';
+
+const tableDUmmy = [
+  {
+    company:"1111",
+    country:"222",
+    contact:"99999999999"
+  },
+  {
+    company:"1111",
+    country:"222",
+    contact:"99999999999"
+  },
+  {
+    company:"1111",
+    country:"222",
+    contact:"99999999999"
+  },
+  {
+    company:"1111",
+    country:"222",
+    contact:"99999999999"
+  }
+];
+
+const Report = () => {
+  
+  const [table, setTable] = useState(tableDUmmy);
+
+  const [state, setState] = useState({
+    message: '',
+    post: ''
+  });
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const response = await APIService.getData(state.post);
+    if(response.ok) {
+      setState({
+        message: 'posted successfully',
+        post: ''
+      })
+    }
+  }
+
+  return (
+    <div className="report">
+      Report Component
+
+      <table border="1" rules="all" cellspacing="5" cellpadding="5" width="100%">
+        <thead>
+        <tr>
+          <th>Company</th>
+          <th>Contact</th>
+          <th>Country</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td>Germany</td>
+        </tr>
+        {table.map((tr, index) => (
+          <tr key={index}>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
+
+      <br/>
+      <br/>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+            Body:
+          <input type="text" />
+        </label>
+        <input type="submit" value="Post" />
+      </form>
+
+      <br/>
+      <br/>
 
 
-    <pre>
-      <UserContextProvider>
-        <Grandparent />
-      </UserContextProvider>
-    </pre>
+      <pre>
+        <UserContextProvider>
+          <Grandparent />
+        </UserContextProvider>
+      </pre>
 
 
-  </div>
-);
+    </div>
+  );
+}
 
 Report.propTypes = {};
 
--- react/my-project/src/pages/report/report.test.js
+++ react/my-project/src/pages/report/report.test.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { fireEvent, wait } from '@testing-library/react';
 import { createRoot } from 'react-dom/client';
 import Report from './Report';
+
+import * as APIService from './api';
+jest.mock('./api');
 
 it('It should mount', () => {
   const div = document.createElement('div');
   render(<Report />, div);
   createRoot(div).unmount();
 });
+
+
+describe('Scoped / Nested block', () => {
+  beforeAll(() => console.log('2 - beforeAll'));
+  afterAll(() => console.log('2 - afterAll'));
+  beforeEach(() => console.log('2 - beforeEach'));
+  afterEach(() => console.log('2 - afterEach'));
+
+  test('', () => console.log('2 - test'));
+});
+
+test.only('form makes a api call with proper params', async  () => {
+  APIService.getData.mockResolvedValueOnce({ok : true});
+  const {getByText, getByLabelText, debug} = render(<Report/>);
+  const inputField = getByLabelText(/Body:/i);
+  const submitBtn = getByText(/Post/i);
+  //fireEvent.change(inputField, {'target': {'value' : 'sample title'}});
+  fireEvent.click(submitBtn);
+  //debug(submitBtn);
+  //expect(APIService.getData).toHaveBeenCalledTimes(1)
+  //expect(APIService.getData).toHaveBeenCalledWith("sample title")
+  //await wait(() => null)
+})
